290626b5ba42d38b1edb0c1f081537483c0519c0
Jim McGee <jmcgee86@gmail.com>
2018-07-15 17:06:28 -0400

    half done for pair programming

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace pairCoding_v2
+{
+    abstract class Publication
+    {
+        public DateTime PublishDate {get; set;} 
+
+        abstract public DateTime DueDate {get;}
+
+        abstract public double fee {get;}
+
+        public DateTime DayReturned {get; set;}
+
+        abstract public int checkoutLength {get;}
+
+        public  double lateFee()
+        {
+            var late = this.DaysLate();
+            if (late>0)
+            return fee  * late;
+            else
+                return 0;
+        }
+
+         public int DaysLate()
+        {
+            TimeSpan difference = DayReturned.Date - DueDate.Date;
+            var days = (int) difference.TotalDays;
+            Console.WriteLine("Days Late: " + days);
+            return days;
+        }
+    }
+
+    class Book : Publication
+    {
+        public string Author {get; set;}
+        public string Title {get; set;}
+
+        public override int checkoutLength 
+        {
+            get{return 7;}
+        }
+
+        public override double fee
+        {
+            get {return .25;}
+        }
+
+        public override DateTime DueDate
+        {
+            get
+            {
+               return DateTime.Now.AddDays(checkoutLength);
+            }
+        }
+        public Book ()
+        {
+        }
+
+        public Book (string _author, string _title, DateTime _date)
+        {
+            this.Author = _author;
+            this.Title = _title;
+            this.PublishDate = _date;
+        }
+    }
+
+    class Magazine : Publication
+    {
+ 
+ 
+    }
+
+    class Customer
+    {
+        
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# pairCoding_v2
+Create the following:
+
+A Publication class
+    Required data
+        Come up with 1 attributes needed by all publication types
+    Required method
+        calculate late fee
+        calculate due date
+A book class
+    Required data:
+        author
+        title
+        how long can it be checked out
+        late fee per day late
+A magazine  class
+    Required Data:
+        title
+        how long can it be checked out
+        late fee
+A Customer Class
+    required data
+        name
+        age
+        fines due
+        items currently checked out
+    required method
+        library transaction (checkout one item or return all items)
+        pay fine
+
+Implement a Queue for publications checked out by customer
+    This queue should store all items checked out in a queue until they are returned.
+    when items are returned, should check it item was returned late and add any fines due to customer.
